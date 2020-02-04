a2b9e3f1ad9fee7e77de4839e9fd90b3a7477007
Otto Cedeno <otto@ottostacos.com>
2020-02-04 03:17:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,7 +71,7 @@ class Student
   def self.first_X_students_in_grade_10(num_of_students)
     sql = "SELECT * FROM Students WHERE grade = 10 LIMIT #{num_of_students}"
 
-    DB[:conn]
+    DB[:conn].execute(sql)
   end
 
 end
