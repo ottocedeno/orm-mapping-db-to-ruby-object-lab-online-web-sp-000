267b7fff7f77b43bd89f76762759e6050b258599
Otto Cedeno <otto@ottostacos.com>
2020-02-04 03:16:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -68,7 +68,7 @@ class Student
     DB[:conn].execute(sql).map {|student| self.new_from_db(student)}
   end
 
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10(num_of_students)
     sql = "SELECT * FROM Students"
 
   end
