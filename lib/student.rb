443d104338b699f06e9c153d3154bb5a637dcd18
Otto Cedeno <otto@ottostacos.com>
2020-02-04 03:14:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,7 +65,7 @@ class Student
   def self.students_below_12th_grade
     sql = "SELECT * FROM Students WHERE grade < 12"
 
-    DB[:conn].execute(sql).map
+    DB[:conn].execute(sql).map {|student| self.new_from_db(student)}
   end
 
 end
